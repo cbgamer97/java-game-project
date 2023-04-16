2a12e625cbea320781ddc8efa33dd3b2ca12a682
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-04-16 02:01:40 -0500

    working on background tile loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.Player;
+import tile.TileManager;
 
 import javax.swing.JPanel;
 import java.awt.*;
@@ -14,23 +15,18 @@ public class GamePanel extends JPanel implements Runnable{
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale; // 48x48 TILE
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol; // 768 PIXELS
-    final int screenHeight = tileSize * maxScreenRow; // 576 PIXELS
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol; // 768 PIXELS
+    public final int screenHeight = tileSize * maxScreenRow; // 576 PIXELS
 
     // FPS
     int FPS = 60;
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
 
     Player player = new Player(this, keyH);
-
-    // SET PLAYERS DEFAULT POSITION
-
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
 
     public GamePanel() {
 
@@ -125,8 +121,10 @@ public class GamePanel extends JPanel implements Runnable{
 
         Graphics2D g2 = (Graphics2D) g;
 
+        tileM.draw(g2);
         player.draw(g2);
 
         g2.dispose();
+
     }
 }
