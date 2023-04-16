65f2ea8bcb30845adcd387554bcfcd766be81652
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-04-16 01:29:08 -0500

    solved issue with images on player

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -2,19 +2,18 @@ package entity;
 
 import main.GamePanel;
 import main.KeyHandler;
-
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
-import java.util.Objects;
 
 public class Player extends Entity{
 
     GamePanel gp;
     KeyHandler keyH;
 
-    public Player(GamePanel gp, KeyHandler KeyH) {
+    public Player(GamePanel gp, KeyHandler keyH) {
 
         this.gp = gp;
         this.keyH = keyH;
@@ -27,20 +26,20 @@ public class Player extends Entity{
 
         x = 100;
         y = 100;
-        speed = 4;
+        speed = 3;
         direction = "down";
     }
 
     public void getPlayerImage() {
         try{
-            up1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/res/boy_left_2.png"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+            up1 = ImageIO.read(new File("./res/boy_up_1.png"));
+            up2 = ImageIO.read(new File("./res/boy_up_2.png"));
+            down1 = ImageIO.read(new File("./res/boy_down_1.png"));
+            down2 = ImageIO.read(new File("./res/boy_down_2.png"));
+            left1 = ImageIO.read(new File("./res/boy_left_1.png"));
+            left2 = ImageIO.read(new File("./res/boy_left_2.png"));
+            right1 = ImageIO.read(new File("./res/boy_right_1.png"));
+            right2 = ImageIO.read(new File("./res/boy_right_2.png"));
         }catch(IOException e){
             e.printStackTrace();
         }
@@ -56,7 +55,7 @@ public class Player extends Entity{
         } else if(keyH.leftPressed == true){
             direction = "left";
             x -= speed;
-        } else if (keyH.rightPressed == true) {
+        } else if (keyH.rightPressed = true) {
             direction = "right";
             x += speed;
         }
@@ -64,25 +63,24 @@ public class Player extends Entity{
 
     public void draw(Graphics2D g2) {
 
-//        g2.setColor(Color.white);
-//
-//        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
-        BufferedImage image =null;
 
-        switch (direction) {
-            case "up":
-                image = up1;
-                break;
-            case "down":
-                image = down1;
-                break;
-            case "left":
-                image = left1;
-                break;
-            case "right":
-                image = right1;
-                break;
-        }
+        BufferedImage image = null;
+
+            switch (direction) {
+                case "up":
+                    image = up1;
+                    break;
+                case "down":
+                    image = down1;
+                    break;
+                case "left":
+                    image = left1;
+                    break;
+                case "right":
+                    image = right1;
+                    break;
+            }
+
         g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
 
     }
