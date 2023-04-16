02b571165abb579f1650b4033e7dfae9ee271d53
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-04-15 21:17:59 -0500

    completed character movement

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,7 +2,6 @@ package main;
 
 import javax.swing.JPanel;
 import java.awt.*;
-
 public class GamePanel extends JPanel implements Runnable{
     // SCREEN SETTINGS
     final int originalTileSize = 16; //16x16 tile
@@ -15,13 +14,25 @@ public class GamePanel extends JPanel implements Runnable{
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768 PIXELS
     final int screenHeight = tileSize * maxScreenRow; // 576 PIXELS
+
+    // FPS
+    int FPS = 60;
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    // SET PLAYERS DEFAULT POSITION
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -32,22 +43,48 @@ public class GamePanel extends JPanel implements Runnable{
     @Override
     public void run() {
 
+        double drawInterval = 1000000000/FPS; //0.016 SECONDS
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
         while(gameThread != null) {
 
-            System.out.println("The game loop is running");
-
-            // 1 UPDATE: UPDATE INFORMATION SUCH AS CHATACTER POSITIONS
+            // 1 UPDATE: UPDATE INFORMATION SUCH AS CHARACTER POSITIONS
 
             update();
 
             // 2 DRAW: DRAW THE SCREEN WITH THE UPDATED INFORMATION
 
             repaint();
+
+
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+
+                if(remainingTime < 0){
+                    remainingTime = 0;
+                }
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
         }
     }
 
     public void update () {
 
+        if(keyH.upPressed == true){
+            playerY -= playerSpeed;
+        } else if(keyH.downPressed == true){
+            playerY += playerSpeed;
+        } else if(keyH.leftPressed == true){
+            playerX -= playerSpeed;
+        } else if (keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
     }
 
     public void paintComponent(Graphics g) {
@@ -58,7 +95,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         g2.setColor(Color.white);
 
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         g2.dispose();
     }
