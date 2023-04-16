7a073ad99a0e2b3852c7a63afe245293d1ad68cd
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-04-15 20:46:30 -0500

    made key handling class

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,7 +3,7 @@ package main;
 import javax.swing.JPanel;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
     // SCREEN SETTINGS
     final int originalTileSize = 16; //16x16 tile
 
@@ -15,11 +15,51 @@ public class GamePanel extends JPanel {
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768 PIXELS
     final int screenHeight = tileSize * maxScreenRow; // 576 PIXELS
+    Thread gameThread;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run() {
+
+        while(gameThread != null) {
+
+            System.out.println("The game loop is running");
+
+            // 1 UPDATE: UPDATE INFORMATION SUCH AS CHATACTER POSITIONS
+
+            update();
+
+            // 2 DRAW: DRAW THE SCREEN WITH THE UPDATED INFORMATION
+
+            repaint();
+        }
+    }
+
+    public void update () {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(100, 100, tileSize, tileSize);
+
+        g2.dispose();
     }
 }
