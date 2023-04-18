ada9627cd4104e0e6085477dd254d00d35cff188
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-04-17 21:49:19 -0500

    added sound to game and sound effects to items

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -124,18 +124,21 @@ public class Player extends Entity{
 
             switch (objectName){
                 case "Key":
+                    gp.playSE(1);
                     hasKey++;
                     gp.obj[i] = null;
                     System.out.println("Keys: " + hasKey);
                     break;
                 case "Door":
                     if(hasKey > 0) {
+                        gp.playSE(3);
                         gp.obj[i] = null;
                         System.out.println("Keys: " + hasKey);
                         hasKey--;
                     }
                     break;
                 case "Boots":
+                    gp.playSE(2);
                     speed += 2;
                     gp.obj[i] = null;
                     break;
