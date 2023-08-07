97d4b662b9a4ac2b2fdb9d5ee27296c17b752c13
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-08-06 19:09:32 -0500

    adjusting tile sizes

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -15,8 +15,8 @@ public class GamePanel extends JPanel implements Runnable{
     final int scale = 3;
 
     public final int tileSize = originalTileSize * scale; // 48x48 TILE
-    public final int maxScreenCol = 16;
-    public final int maxScreenRow = 12;
+    public final int maxScreenCol = 32;
+    public final int maxScreenRow = 24;
     public final int screenWidth = tileSize * maxScreenCol; // 768 PIXELS
     public final int screenHeight = tileSize * maxScreenRow; // 576 PIXELS
 
